9e00b4d89c85f3d63b47de4feaf626dc0f5d86eb
Logan Donley <ldonley@cloudbees.com>
2019-11-06 14:34:18 -0500

    Fixing missing parren

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -9,7 +9,7 @@ def call(Map config) {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
         }
         else if (config.deployType == "vmware") {
-          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig /.config/kube/config
+          sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig /.config/kube/config"
         }
         else {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --allow-unauthenticated --region ${config.region} --platform managed"
