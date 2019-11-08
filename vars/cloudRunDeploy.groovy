3fa669e3693e7204d85d68b99f53472ecb6ed891
Logan Donley <ldonley@cloudbees.com>
2019-11-08 10:16:46 -0500

    Adding debug sleep

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -9,7 +9,7 @@ def call(Map config) {
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform gke --cluster ${config.clusterName} --cluster-location ${config.region} --namespace ${config.namespace}"
         }
         else if (config.deployType == "vmware") {
-          sh "env"
+          sleep(300)
           sh "gcloud auth login"
           sh "gcloud beta run deploy ${config.serviceName} --image ${config.image} --platform kubernetes --namespace ${config.namespace} --kubeconfig /.config/kube/config"
         }
