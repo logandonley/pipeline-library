79401cd5dd5d6e6c3418f6aacb4237ad10cd92d5
Logan Donley <ldonley@cloudbees.com>
2019-11-08 11:47:55 -0500

    Testing

--- vars/kanikoBuildPushGeneric.groovy
+++ vars/kanikoBuildPushGeneric.groovy
@@ -10,6 +10,7 @@ def call(String imageName, String imageTag = env.BUILD_NUMBER, String gcpProject
       gitShortCommit()
       container(name: 'kaniko', shell: '/busybox/sh') {
         withEnv(['PATH+EXTRA=/busybox:/kaniko']) {
+          sh "cat /kaniko/.docker/config.json"
           sh """#!/busybox/sh
             /kaniko/executor -f ${pwd()}/${dockerFile} -c ${pwd()} --insecure --skip-tls-verify --cache=true --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor=${env.COMMIT_AUTHOR} -d ldonleycb/test-blog:${imageTag}
           """
