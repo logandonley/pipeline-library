6b87532bc031561464c28f6f48f809fe2201bac6
Logan Donley <ldonley@cloudbees.com>
2020-01-30 08:22:45 -0500

    Adding imgBuildNexus

---
+++ vars/imgBuildNexus.groovy
@@ -0,0 +1,18 @@
+// vars/imgBuildNexus.groovy
+def call(String imageName, String imageTag = env.BUILD_NUMBER, String target = ".", String dockerFile="Dockerfile", Closure body) {
+  def dockerReg = "workshop"
+  def label = "img-${UUID.randomUUID().toString()}"
+  def podYaml = libraryResource 'podtemplates/imageBuildPushNexus.yml'
+  podTemplate(name: 'img', label: label, yaml: podYaml) {
+    node(label) {
+      body()
+      imageNameTag()
+      gitShortCommit()
+      container('img') {
+        sh """
+          img build --build-arg buildNumber=${BUILD_NUMBER} --build-arg shortCommit=${env.SHORT_COMMIT} --build-arg commitAuthor="${env.COMMIT_AUTHOR}" -t ${dockerReg}/${imageName}:${imageTag} ${pwd()}
+        """
+      }
+    }
+  }
+}
